78dad189ec37126ca2e23b6dfafde80e7c3bd7f9
Raddames <raddamestonui48@gmail.com>
2024-05-21 22:41:38 +0300

    completed

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+# def say_hi(name):
+#     print(f"Hi there, {name}!")
+
+# say_hi()
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+print(stylish_painter())
